c5ebfe47d33c88e434af9ce3ec7a1542834bbbb7
ing.dacastillop <ing.dacastillop@gmail.com>
2023-09-13 11:06:48 -0500

    :bento: Handle response permission when default case

--- ios/Plugin/class/Permissions.swift
+++ ios/Plugin/class/Permissions.swift
@@ -5,21 +5,24 @@ class Permissions {
 
     class func contactPermission(completionHandler: @escaping (_ accessGranted: Bool) -> Void) {
         let contactStore = CNContactStore()
+
         switch CNContactStore.authorizationStatus(for: .contacts) {
-        case .authorized:
-            completionHandler(true)
-        case .denied:
-            completionHandler(false)
-        case .restricted, .notDetermined:
-            contactStore.requestAccess(for: .contacts) { granted, _ in
-                if granted {
-                    completionHandler(true)
-                } else {
-                    DispatchQueue.main.async {
-                        completionHandler(false)
+            case .authorized:
+                completionHandler(true)
+            case .denied:
+                completionHandler(false)
+            case .restricted, .notDetermined:
+                contactStore.requestAccess(for: .contacts) { granted, _ in
+                    if granted {
+                        completionHandler(true)
+                    } else {
+                        DispatchQueue.main.async {
+                            completionHandler(false)
+                        }
                     }
                 }
-            }
+            default:
+                completionHandler(false)
         }
     }
 }
